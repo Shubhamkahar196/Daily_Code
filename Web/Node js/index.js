c46748dae86e6396b0bc3a697697a9e8c72c9bd8
Shubham kahar <Shubhamkahar196@gmail.com>
2024-12-07 05:40:03 +0530

    Making CLI

--- Web/Node js/index.js
+++ Web/Node js/index.js
@@ -1,42 +1,25 @@
 const fs = require('fs');
+const { Command } = require('commander');
+const program = new Command();
 
-function main(fileName){
-    // console.log(process.argv);
-    let total = 0;
-    fs.readFile(fileName, "utf-8", (err,data)=>{
-        for(let i = 0; i<data.length;i++){
-            if(data[i] === " "){
-                total++;
-            }
-        }
-        console.log(total + 1);
-    })
-}
+program
+  .name('counter')
+  .description('CLI to do file based tasks')
+  .version('0.8.0');
 
-main("a.txt");
-// main(process.argv[2]);
+program.command('count')
+  .description('Count the number of words in a file')
+  .argument('<file>', 'file to count')
+  .action((file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        // const line = data.split('\n').length; // this is for line counting
+         const words = data.trim().split(/\s+/).length;
+        console.log(`There are ${words} words in ${file}`);
+      }
+    });
+  });
 
-// const fs = require('fs');
-// const { Command } = require('commander');
-// const program = new Command();
-
-// program
-//   .name('counter')
-//   .description('CLI to do file based tasks')
-//   .version('0.8.0');
-
-// program.command('count')
-//   .description('Count the number of lines in a file')
-//   .argument('<file>', 'file to count')
-//   .action((file) => {
-//     fs.readFile(file, 'utf8', (err, data) => {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         const lines = data.split('\n').length;
-//         console.log(`There are ${lines} lines in ${file}`);
-//       }
-//     });
-//   });
-
-// program.parse();
+program.parse();
