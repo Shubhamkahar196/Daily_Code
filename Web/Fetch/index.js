3f147d70fee301ff3f5aef10e62094cb199b3870
Shubham kahar <Shubhamkahar196@gmail.com>
2024-12-12 11:33:54 +0530

    Dynamic HTTP Server

--- Web/Fetch/index.js
+++ Web/Fetch/index.js
@@ -1,36 +1,50 @@
 const express = require("express");
 const app = express();
 
-app.get("/sum",(req,res)=>{
-    const a = req.query.a;
-    const b = req.query.b;
+//simple  // http://localhost:3000/sum?a=5&b=8
+// app.get("/sum",(req,res)=>{
+//     const a = pasreInt(req.query.a);
+//     const b = pasreInt(req.query.b);
+
+//     res.json({
+//         ans: a+b
+//     });
+// })
+
+//making it dynamic   
+
+//http://localhost:3000/sum/10/20
+
+app.get("/sum/:a/:b",(req,res)=>{
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
 
     res.json({
         ans: a+b
     });
 })
 
-app.get("/sub",(req,res)=>{
-    const a = req.query.a;
-    const b = req.query.b;
+app.get("/sub/:a/:b",(req,res)=>{
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
 
     res.json({
         ans: a-b
     })
 })
 
-app.get("/mul",(req,res)=>{
-    const a = req.query.a;
-    const b = req.query.b;
+app.get("/mul/:a/:b",(req,res)=>{
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
 
     res.json({
         ans : a*b
     })
 })
 
-app.get("/div",(req,res)=>{
-    const a = req.query.a;
-    const b = req.query.b;
+app.get("/div/:a/:b",(req,res)=>{
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
 
     res.json({
         ans : a/b
