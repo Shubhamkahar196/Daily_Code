8e62aec2ed47ede5451e1d6fb24a83fafb55d9bf
Shubham kahar <Shubhamkahar196@gmail.com>
2024-12-18 20:27:35 +0530

    Authentication

---
+++ Web/Authentication/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/signup",(req,res)=>{
+
+});
+
+app.post("/signin",(req,res)=>{
+
+});
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
