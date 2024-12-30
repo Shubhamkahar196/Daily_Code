354845a44db5e7d2e590e1d61903072599ea5bcf
Shubham kahar <Shubhamkahar196@gmail.com>
2024-12-30 22:06:58 +0530

    Making backend for todo app

---
+++ Web/Todo app/backend/route/todo.js
@@ -0,0 +1,44 @@
+let todos = [];  // in memory
+let currentId = 1;
+
+export async function getAllTodo(req,res,next){
+    res.json(todos);
+}
+
+export async function createTodo(req,res,next){
+    const {task} = req.body;
+    if(!task){
+        return res.status(400).json({
+            error: "Task is required"
+        })
+    }
+    const newTodo = {id: currentId++, task};
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
+}
+
+export async function updateTodo(req,res,next){
+    const { id } = req.params;
+    const { task } = req.body;
+
+    if(!task){
+        return res.status(400).json({
+            error: "Task is required"
+        })
+    }
+
+    const todoIndex = todos.findIndex( todo => todo.id == id);
+    if(todoIndex !==-1){
+        todos[todoIndex] = { ...todos[todoIndex],task};
+        res.json(todos[todoIndex]);
+    }else{
+        res.status(404).json({
+            message: "Todo not found"
+        })
+    }
+}
+
+    
+export async function deleteTodoById(req,res,next){
+
+}
