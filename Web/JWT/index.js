9b07ae650d008edcea538568b36e73194ff7b42c
Shubham kahar <Shubhamkahar196@gmail.com>
2025-01-02 20:01:39 +0530

    JWT with verify

--- Web/JWT/index.js
+++ Web/JWT/index.js
@@ -7,7 +7,10 @@ const value = {
     accountNumber: 123123123
 }
 
-//jwt
-const token = jwt.generate(value, "secret");
-console.log(token);
+//sign not generate
+// const token = jwt.sign(value, "secret");
+// console.log(token);
+
+const verifiedValue = jwt.verify("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoic2h1YmhhbSIsImFjY291bnROdW1iZXIiOjEyMzEyMzEyMywiaWF0IjoxNzM1ODI2NDA1fQ.blJnvRjjNbZt8r9wq9iz4p6qFqtFbXIDyxvkw19e00o","secret");
+console.log(verifiedValue);
 
