9cd6e3de8d44d7bb46b0c945a9a41f032e4732f0
Shubham kahar <Shubhamkahar196@gmail.com>
2025-01-03 11:48:47 +0530

    Making signup in todo app with database

--- Web/Mongodb/index.js
+++ Web/Mongodb/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const { UserModel, TodoModel} = require("./db");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const JWT_SECRET = "shubham@123";
 
+mongoose.connect("mongodb+srv://yt727883:wTg5f7j1Ju3y0iQN@cluster0.1tsxm.mongodb.net/Todo-App-database");
 const app = express();
+app.use(express.json());
 
-app.post("/signup",(req,res) =>{
+app.post("/signup", async function(req,res){
     const email = req.body.email;
     const password = req.body.password;
     const name = req.body.name;
 
-    UserModel.insertMany({
+    await  UserModel.create({
         email: email,
         password: password,
         name: name
@@ -19,8 +24,29 @@ app.post("/signup",(req,res) =>{
     })
 });
 
-app.post("signin", (req,res)=>{
+app.post("signin",async function(req,res){
+   const email = req.body.email;
+   const password = req.body.password;
 
+   const user = UserModel.findOne({
+    email:email,
+    password: password
+   })
+    
+   console.log(user);
+
+   if(user){
+    const token = jwt.sign({
+    id: user._id
+    });
+    res.json({
+        token: token
+    });
+   }else{
+    res.status(403).json({
+        message: "Incorrect Credentials"
+    })
+   }
 });
 
 app.post("/todo",(req,res)=>{
