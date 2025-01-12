18ee0c719ae81012a7e86ce0a8195e2bb0d5cae4
Shubham kahar <Shubhamkahar196@gmail.com>
2025-01-12 23:56:53 +0530

    Adding signup routes with input validation

--- Project/Backend of Course Selling App/index.js
+++ Project/Backend of Course Selling App/index.js
@@ -11,6 +11,7 @@ const app = express();
 
 
 
+app.use(express.json());
 app.use("/api/v1/user",userRouter);
 app.use("/api/v1/admin",adminRouter);
 app.use("/api/v1/course",courseRouter);
--- Project/Backend of Course Selling App/routes/user.js
+++ Project/Backend of Course Selling App/routes/user.js
@@ -2,33 +2,130 @@ const { Router} = require("express");
 const { userModel} = require("../db");
 const userRouter = Router();
 
-    
+
+
+
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "aashvba";
+const bcrypt = require("bcrypt");
+const z = require("zod");
+
 userRouter.post("/signup", async function(req,res){
-    // const { email, password, FirstName, LastName } = req.body; // adding zod validation
-    const email = req.body.email;
-    const password = req.body.password;
-    const FirstName = req.body.FirstName;
-    const LastName = req.body.LastName;
-    //hash the password  tp plaintext pw is not stored in the DB by bcrypt
-       
-    // todo : put inside a try catch block
+    
+    // input validation using zod
+
+    const requireBody = z.object({
+        email: z.string().min(3).max(50).email(),
+        password: z.string().min(3).max(50),
+        firstName: z.string().min(3).max(30),
+        lastName: z.string().min(3).max(30),
+    });
+
+       // Parse the request body using the requireBody.safeParse() method to validate the data format
+    // "safe" parsing (doesn't throw error if validation fails)
+      
+    const parseDataSuccess = requireBody.safeParse(req.body);
+
+    //if data is not correct then yeh response return krdo
+
+    if(!parseDataSuccess.success){
+        return res.json({
+            message: "Incorrect Format",
+            error: parseDataSuccess.error
+        })
+    }
+
+    
+    // Extract validated email, password, firstName, and lastName from the request body
+    const { email,password,FirstName,LastName} = req.body;
+
+      // Hash the user's password using bcrypt with a salt rounds of 5
+       const hashedPassword = await bcrypt.hash(password,5);
+
+
+
+     // creating a new user in the database
+     try {
+        // Create a new user entry with the provided email,hashed password, firstName, last name
+
+   
         await  userModel.create({
-            email : email,
-            password : password,
-            FirstName : FirstName,
-            LastName : LastName
-           })
-   
-   
+            email,
+            password : hashedPassword,
+            FirstName,
+            LastName,
+           });
+     }catch(e){
+         // If there is an error during user creation, return a error message
+         return res.status(400).json({
+            // Provide a message indicating signup failure
+            message: "You are already signup",
+        });
+     }
+      
+     // send a success response back to client indication successfully signup
 
         res.status(200).json({
             message: "You are successfully signup"
         })
     });
+
+
+// Post router for user to Signup
+// userRouter.post("/signup", async function(req, res){      
+
+//     //input validation using zod 
+//     const requiredBody = z.object({
+//         email: z.string().min(3).max(100).email(),
+//         password: z.string().min(3).max(100),
+//         firstName: z.string().min(3).max(30),
+//         lastName: z.string().min(3).max(30),
+//     });
+
+//     // Parse the request body using the requireBody.safeParse() method to validate the data format
+//     // "safe" parsing (doesn't throw error if validation fails)
+//     const parsedDataSuccess = requiredBody.safeParse(req.body);         
+
+//     //If data is not correct then yeh response return kr do
+//     if(!parsedDataSuccess.success){                     
+//         return res.json({
+//             message: "Incorrect Format",
+//             error: parsedDataSuccess.error
+//         })
+//     }
+
+//      // Extract validated email, password, firstName, and lastName from the request body
+//      const {email, password, firstName, lastName} = req.body;
+
+//      // Hash the user's password using bcrypt with a salt rounds of 5
+//      const hashedPassword = await bcrypt.hash(password, 5)
+     
+//      // Creating a new user in the database
+//      try{
+//          // Create a new user entry with the provided email, hashed password, firstName, lastName
+//          await userModel.create({
+//              email,
+//              password: hashedPassword,
+//              firstName,
+//              lastName,   
+//          });
+//      } catch(e){
+//          // If there is an error during user creation, return a error message
+//          return res.status(400).json({
+//              // Provide a message indicating signup failure
+//              message: "You are already signup",
+//          });
+//      }
+
+//      // Send a success response back to client indicating successfully singup
+//     res.json({
+//         message: "Signed up Successfull"
+//     });
+// });
     
 userRouter.post("/signin",function(req,res){
-             const email = req.params.email;
-             const password = req.params.password;
+            //  const email = req.params.email;
+            //  const password = req.params.password;
     
              res.status(200).json({
                 message: "You are logged in"
