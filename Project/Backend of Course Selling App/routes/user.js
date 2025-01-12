0bae8452a541681a063667c9132f466b84f62c11
Shubham kahar <Shubhamkahar196@gmail.com>
2025-01-12 23:58:46 +0530

    Making signin route

--- Project/Backend of Course Selling App/routes/user.js
+++ Project/Backend of Course Selling App/routes/user.js
@@ -71,61 +71,11 @@ userRouter.post("/signup", async function(req,res){
     });
 
 
-// Post router for user to Signup
-// userRouter.post("/signup", async function(req, res){      
 
-//     //input validation using zod 
-//     const requiredBody = z.object({
-//         email: z.string().min(3).max(100).email(),
-//         password: z.string().min(3).max(100),
-//         firstName: z.string().min(3).max(30),
-//         lastName: z.string().min(3).max(30),
-//     });
-
-//     // Parse the request body using the requireBody.safeParse() method to validate the data format
-//     // "safe" parsing (doesn't throw error if validation fails)
-//     const parsedDataSuccess = requiredBody.safeParse(req.body);         
-
-//     //If data is not correct then yeh response return kr do
-//     if(!parsedDataSuccess.success){                     
-//         return res.json({
-//             message: "Incorrect Format",
-//             error: parsedDataSuccess.error
-//         })
-//     }
-
-//      // Extract validated email, password, firstName, and lastName from the request body
-//      const {email, password, firstName, lastName} = req.body;
-
-//      // Hash the user's password using bcrypt with a salt rounds of 5
-//      const hashedPassword = await bcrypt.hash(password, 5)
-     
-//      // Creating a new user in the database
-//      try{
-//          // Create a new user entry with the provided email, hashed password, firstName, lastName
-//          await userModel.create({
-//              email,
-//              password: hashedPassword,
-//              firstName,
-//              lastName,   
-//          });
-//      } catch(e){
-//          // If there is an error during user creation, return a error message
-//          return res.status(400).json({
-//              // Provide a message indicating signup failure
-//              message: "You are already signup",
-//          });
-//      }
-
-//      // Send a success response back to client indicating successfully singup
-//     res.json({
-//         message: "Signed up Successfull"
-//     });
-// });
     
-userRouter.post("/signin",function(req,res){
-            //  const email = req.params.email;
-            //  const password = req.params.password;
+userRouter.post("/signin", async function(req,res){
+             const email = req.params.email;
+             const password = req.params.password;
     
              res.status(200).json({
                 message: "You are logged in"
