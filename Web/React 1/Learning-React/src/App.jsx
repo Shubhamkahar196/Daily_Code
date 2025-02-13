bb15f838d47f46377e45b802328fee17a59adfe8
Shubham kahar <Shubhamkahar196@gmail.com>
2025-02-13 20:27:05 +0530

    Making a Post Component

--- Web/React 1/Learning-React/src/App.jsx
+++ Web/React 1/Learning-React/src/App.jsx
@@ -3,15 +3,42 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [PostComponent] = useState(0)
 
   return (
-    <>
-      <div>
-      <h1>Learning React </h1>
+   
+      <div> 
+      <PostComponent></PostComponent>
+      
       </div> 
-    </>
+    
   )
+}
+
+const style = { width:200, backgroundColor: "blue", borderRadius: 10, borderColor: "gray", borderWidth: 1, display: "flex"}
+
+function PostComponent() {
+  return  <div>
+
+  
+  <div style={style}> 
+   <img src={"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"} style={{
+    width: 20,
+    height: 20,
+    borderRadius: 20
+   }}  />
+    <div style={{fontSize: 10}}>
+    <b>100xdevs</b>
+    <div> 23,888 followers </div> 
+    <div> 12m </div> 
+    </div>
+     </div>
+     <div style={{fontSize: 12}}>
+      what to know how to win big? Check out how these folks won $6000 in bounties.
+     </div>
+     </div>
+    
+     
 }
 
 export default App
