4b6c96d72bc71ef397ad9d1831274aeb1b9a0a37
Shubham kahar <Shubhamkahar196@gmail.com>
2025-03-25 23:01:27 +0530

    Initial commit

--- Web/React 2/React_part2/src/App.jsx
+++ Web/React 2/React_part2/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [ count , setCount] = useState(0)
+                
   return (
     <>
-       <h1>React part 2</h1>
+       <h1> React_part 2 </h1>
+       <p> Learning React part 2</p>
     </>
   )
 }
