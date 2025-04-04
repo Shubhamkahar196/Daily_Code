1e425f128b2f987c2bd3611a8e271427650935c2
Shubham kahar <Shubhamkahar196@gmail.com>
2025-04-04 20:57:50 +0530

    SPA_Routing

--- Web/React 2/React_part2/SPA_Routing/src/App.jsx
+++ Web/React 2/React_part2/SPA_Routing/src/App.jsx
@@ -1,18 +1,25 @@
 
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route,Link} from 'react-router-dom'
 // import this
 import './App.css'
 
 function App() {
   return <div>
-   <a href='/'>Allen</a>  
-   | 
-   <a href='/neet/online-coaching-class-11'a>Class 11 </a>  
+   {/* <a href='/'>Allen</a>   */}
 
-   |
-    <a href="/">Class 12</a> 
+  
+   {/* |  */}
+   {/* <a href='/neet/online-coaching-class-11'a>Class 11 </a>   */}
+
+   {/* | */}
+    {/* <a href="/">Class 12</a>  */}
 
     <BrowserRouter>
+    <Link to="/">Allen</Link>
+    |
+   <Link to="/neet/online-coaching-class-12">Class 12</Link>
+   |
+   <Link to="/neet/online-coaching-class-11">Class 11</Link>
     <Routes>
       <Route path='/neet/online-coaching-class-11' element={<Class11Program/>}> </Route>
       <Route path='/neet/online-coaching-class-12' element={<Class12Program/>}> </Route>
