7d84cbb104aa9d40e4ae7879b8b050594ce6e18f
Shubham kahar <Shubhamkahar196@gmail.com>
2025-04-04 21:08:23 +0530

    Completed Learning about BrowserRouter, Routes, Route, Link , UseNavigate , redirect

--- Web/React 2/React_part2/SPA_Routing/src/App.jsx
+++ Web/React 2/React_part2/SPA_Routing/src/App.jsx
@@ -1,5 +1,5 @@
 
-import {BrowserRouter, Routes, Route,Link} from 'react-router-dom'
+import {BrowserRouter, Routes, Route,Link, useNavigate, redirect} from 'react-router-dom'
 // import this
 import './App.css'
 
@@ -24,12 +24,18 @@ function App() {
       <Route path='/neet/online-coaching-class-11' element={<Class11Program/>}> </Route>
       <Route path='/neet/online-coaching-class-12' element={<Class12Program/>}> </Route>
       <Route path='/' element={<Landing/>}></Route>
+      <Route path='*' element={<ErrorPage/>}></Route>
     </Routes>
     </BrowserRouter>
   
   </div>
 }
 
+function ErrorPage(){
+  return <>
+     Sorry page not found 
+  </>
+}
 function Landing(){
   return <div>
     Welcome to allen
@@ -37,14 +43,21 @@ function Landing(){
 }
 
 function Class11Program() {
+  
+
   return <div>
     NEET programs for class 11th
   </div>
 }
 
 function Class12Program() {
+  const navigate = useNavigate();
+  function redirectUser (){
+    navigate("/")
+  }
   return <div>
     NEET program for class 12th
+    <button onClick={redirectUser}>Go back to landing page</button>
   </div>
 }
 export default App
