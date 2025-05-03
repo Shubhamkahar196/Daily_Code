64d982c2590917b9d4b8cda932dfd09403811286
Shubham kahar <Shubhamkahar196@gmail.com>
2025-05-03 14:14:50 +0530

    Learning UseFetch and re-fetching

--- Web/week -11/useFecth_Custom_hook_2/src/App.jsx
+++ Web/week -11/useFecth_Custom_hook_2/src/App.jsx
@@ -1,15 +1,33 @@
 import { useState, useEffect } from 'react'
 
 import './App.css'
-import { usePostTitle } from './hooks/useFetch'
+import { usePostTitle,useFetch } from './hooks/useFetch'
 
 function App() {
-const postTitle = usePostTitle();
+// const postTitle = usePostTitle();
+
+const[currentPost, setCurrentPost] = useState(1);
+// calling useFetch from hook 
+const {finalData, loading} = useFetch("https://jsonplaceholder.typicode.com/posts/" + currentPost);
+
+//  first show Loading then show rendering post
+if(loading){
+  return <div>
+    Loading...
+  </div>
+}
   return (
     <>
-      <div>
+    <div> 
+      {/* when i click on button they change  */}
+    <button onClick={()=> setCurrentPost(1)}>1</button>
+    <button onClick={()=> setCurrentPost(2)}>2</button>
+    <button onClick={()=> setCurrentPost(3)}>3</button>
+    
+      
         {/* rendering */}
-       {postTitle}
+       {/* {postTitle} */}
+       {JSON.stringify(finalData)}
       </div>
       
     </>
--- Web/week -11/useFecth_Custom_hook_2/src/hooks/useFetch.js
+++ Web/week -11/useFecth_Custom_hook_2/src/hooks/useFetch.js
@@ -21,20 +21,29 @@ export function usePostTitle(){
 // it a generic custom hook its take url(what you passed in url) and give some data 
 export function useFetch(url){
   const[finalData, setFinalData] = useState({});
-
+  const [loading, setLoading] = useState(true)
+  
 
   async function getDetails(){
+    setLoading(true);    // first true 
    const response = await fetch(url);
    const json = await response.json();
    setFinalData(json)
+   setLoading(false)  // after false 
   }
 
   useEffect(()=>{
     getDetails();
-  }, [])
-
+  }, [url])
+  // url in dependency and they update when the click on button they change the url from 1 to 2 to 3
+ 
+  // they show loading for 10sec 
+  useEffect(() =>{
+    setInterval(getDetails, 10*1000)
+  },[])
 
   return {
-    finalData
+    finalData,
+    loading
   }
 }
