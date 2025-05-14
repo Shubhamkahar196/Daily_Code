eda107a444f993fab8dd0aece0055ab6eeeb4d47
Shubham kahar <Shubhamkahar196@gmail.com>
2025-05-14 23:05:22 +0530

    atom complete through linkend Notification example

--- Web/week -11/Recoil offline DeepDive/Atom/src/App.jsx
+++ Web/week -11/Recoil offline DeepDive/Atom/src/App.jsx
@@ -1,20 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
+import { networkAtom,jobsAtom,messagingAtom,notificationAtom } from './atom'
 
 function App() {
-  const [count, setCount] = useState(0)
+   return <RecoilRoot>
+    <MainApp/>
+   </RecoilRoot>
+
+}
+
+
+function MainApp(){
+  const networkNotificationCount = useRecoilValue(networkAtom);
+  
+const finalValue = networkNotificationCount >= 100 ? "99+" : networkNotificationCount;
+
+const jobsAtomCount = useRecoilValue(jobsAtom);
+const notificationAtomCount = useRecoilValue(notificationAtom);
+// const [messaginAtomCount, setMessagingAtomCount] = useRecoilState(messagingAtom);
+const messaginAtomCount = useRecoilValue(messagingAtom);
 
   return (
     <>
         <button>Home</button>
-        <button>My network</button>
-        <button>Jobs</button>
-        <button>Messaging</button>
-        <button>Notification</button>
+
+        <button>My network ({finalValue})</button>
+        <button>Jobs({jobsAtomCount}</button>
+        <button>Messaging ({messaginAtomCount})</button>
+        <button>Notification ({notificationAtomCount}</button>
+
+       
     </>
   )
+}
+
+function ButtonUpdater(){
+  const [messaginAtomCount, setMessagingAtomCount] = useSetRecoilState(messagingAtom);
+  return <button onClick={() =>{
+          setMessagingAtomCount(messaginAtomCount + 1);
+        }}>Me</button>
 }
 
 export default App
