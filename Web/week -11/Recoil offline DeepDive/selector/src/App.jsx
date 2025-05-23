ea6e6504efb99563e304c8a434e57c5446ce78c8
Shubham kahar <Shubhamkahar196@gmail.com>
2025-05-23 22:15:11 +0530

    fixing selector

--- Web/week -11/Recoil offline DeepDive/selector/src/App.jsx
+++ Web/week -11/Recoil offline DeepDive/selector/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
+import { networkAtom,jobsAtom,messagingAtom,notificationAtom } from './selector'
+import { useMemo } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+   return <RecoilRoot>
+    <MainApp/>
+   </RecoilRoot>
+
+}
+
+
+function MainApp(){
+  const networkNotificationCount = useRecoilValue(networkAtom);
+  
+const finalValue = networkNotificationCount >= 100 ? "99+" : networkNotificationCount;
+
+const jobsAtomCount = useRecoilValue(jobsAtom);
+const notificationAtomCount = useRecoilValue(notificationAtom);
+
+const messaginAtomCount = useRecoilValue(messagingAtom);
+
+// useMemo 
+const totalNotificationCount = useMemo ( ()=>{
+  return  networkNotificationCount + jobsAtomCount + notificationAtomCount + messaginAtomCount ;
+},[networkNotificationCount + jobsAtomCount + notificationAtomCount + messaginAtomCount]) 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <button>Home</button>
+
+        <button>My network ({finalValue})</button>
+        <button>Jobs({jobsAtomCount}</button>
+        <button>Messaging ({messaginAtomCount})</button>
+        <button>Notification ({notificationAtomCount}</button>
+         
+         <button>Me ({totalNotificationCount})</button>
+       
     </>
   )
 }
+
+
 
 export default App
---
+++ Web/week -11/Recoil offline DeepDive/selector/src/selector.js
@@ -0,0 +1,28 @@
+import { atom ,selector} from "recoil";
+
+export const networkAtom = atom({
+    key: "networkAtom",
+    default: 104
+});
+
+export const jobsAtom = atom({
+    key: "jobsAtom",
+    default: 0
+});
+export const notificationAtom = atom({
+    key: "notificationAtom",
+    default: 14
+});
+export const messagingAtom = atom({
+    key: "messagingAtom",
+    default: 104
+});
+
+// creating selector
+
+export const totalNotificationSelector = selector({
+    key: "totalNotificationSelector",
+    value: ({get}) =>{
+        const networkAtom = get(networkAtom);
+    }
+})
