007f3d93583becdb024495abdd84c272384add7e
Shubham kahar <Shubhamkahar196@gmail.com>
2025-05-26 20:31:01 +0530

    making useState in login.jsx

--- Web/React_Project/miniContext/src/components/Login.jsx
+++ Web/React_Project/miniContext/src/components/Login.jsx
@@ -1,9 +1,28 @@
 import React ,{useState, useContext} from 'react'
 import UserContext from '../context/UserContext'
 
+
+
 const Login = () => {
+  const [username, setUsername ] = useState('')
+  const [password, setPassword ] = useState('')
+
+
+  const {setUser} = useContext(UserContext)
+
+
+  const handleSubmit = () =>{
+    
+  }
+
+
   return (
-    <div>Login</div>
+    <div>
+      <h2>Login</h2>
+      <input type="text" placeholder='username' value={username} onChange={ (e) => setUsername(e.target.value)} />
+      <input type="text" placeholder='password' value={password} onChange={ (e) => setPassword(e.target.value)} />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
   )
 }
 
