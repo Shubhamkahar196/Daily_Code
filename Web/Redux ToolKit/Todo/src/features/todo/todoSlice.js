4894df102c3df8cbb74fa857a81e4b24ef0b88b9
Shubham kahar <Shubhamkahar196@gmail.com>
2025-05-30 17:44:41 +0530

    Making reducers

--- Web/Redux ToolKit/Todo/src/features/todo/todoSlice.js
+++ Web/Redux ToolKit/Todo/src/features/todo/todoSlice.js
@@ -1,17 +1,30 @@
-import { createSlice , nanoid} from "@reduxjs/toolkit";
 
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+//state
 const initialState = {
-    todos: [{id: 1, text: "hello world"}]
-}
+  todos: [{ id: 1, text: "hello world" }],
+};
+
+//slice
+export const todoSlice = createSlice({
+  name: 'todo',
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state,action) => {
+     state.todos = state.todos.filter((todo)=> todo.
+    id !==action.payload)
+    },
+  },
+});
 
 
 
-// slice 
-export const  todoSlice = createSlice({
-    name: 'todo',
-    initialState,
-    reducers:{ 
-        addTodo: (state, action) => {},
-        removeTodo: () => {},
-    }
-})
+
