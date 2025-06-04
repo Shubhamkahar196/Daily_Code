48565f0641fd1e1a6cd15480f8e4e24d8f8e602a
Shubham kahar <Shubhamkahar196@gmail.com>
2025-06-04 21:58:36 +0530

    Making userAccount method

--- Project/MegaBlog/src/appwrite/auth.js
+++ Project/MegaBlog/src/appwrite/auth.js
@@ -2,7 +2,38 @@ import conf from "../conf/conf";
 
 import {Client, Account, ID } from "appwrite"
 
-export class AuthService{}
+export class AuthService{
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+           .setEndpoint(conf. appwriteUrl)
+           .setProject(conf.appwriteProjectId)
+
+           this.account = new Account(this.client);
+           
+    }
+  
+    async createAccount({email,password,name}){
+        try {
+        const userAccount = await this.account.create(ID.unique(),email,password,name);
+        if(userAccount){
+            return userAccount;
+            //call another method
+
+
+        }else{
+            return  userAccount;
+        }
+        }catch(error){
+            throw error;
+        }
+    }
+
+    async login
+
+}
 
 
 
