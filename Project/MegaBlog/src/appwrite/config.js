74ec0e254452ab02195c04c6c0c383d9b88c4cce
Shubham kahar <Shubhamkahar196@gmail.com>
2025-06-05 17:08:56 +0530

    Making createpost

--- Project/MegaBlog/src/appwrite/config.js
+++ Project/MegaBlog/src/appwrite/config.js
@@ -10,6 +10,30 @@ export class Service{
 
     constructor(){
         this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client); 
+    }
+
+
+    //create post
+    async createPost({title,slug,content,featuredImage,status,userId}){
+        try{
+           return await this.databases.createDocument(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            slug,{
+                title,
+                content,
+                featuredImage,
+                status,
+                userId,
+            }
+           )
+        }catch(error){
+            throw error;
+        }
     }
 }
 
