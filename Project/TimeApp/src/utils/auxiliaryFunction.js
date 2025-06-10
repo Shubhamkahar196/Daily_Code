48f9022a9fe34130c4214305500600f090f1c808
Shubham kahar <Shubhamkahar196@gmail.com>
2025-06-10 20:46:06 +0530

    Writing code for auxilaryFunction.js

--- Project/TimeApp/src/utils/auxiliaryFunction.js
+++ Project/TimeApp/src/utils/auxiliaryFunction.js
@@ -1,1 +1,19 @@
-export 
+export const formatTime = (totalSeconds) => {
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60)
+    const seconds = totalSeconds % 60;
+
+    return {
+        hours : hours.toString().padStart(2, '0'),
+        minutes : minutes.toString().padStart(2, '0'),
+        seconds : seconds.toString().padStart(2, '0'),
+    }
+}
+
+
+export const calculateTime = (hours, minutes, seconds) =>{
+    const calculateTime = parseInt(hours) * 3600 +
+     parseInt(minutes) * 60 + parseInt(seconds);
+
+     return isNaN(calculateTime) ? 0 : calculateTime; // return 0 if any value is Nan
+}
