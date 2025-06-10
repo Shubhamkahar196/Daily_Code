cb22946b42d150b6d5fdfe1859a5d682e3bbd419
Shubham kahar <Shubhamkahar196@gmail.com>
2025-06-10 20:56:09 +0530

    Writing code for timer.jsx

--- Project/TimeApp/src/components/Timer.jsx
+++ Project/TimeApp/src/components/Timer.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import style from "./Timer.module.css"
+import { formatTime,calculateTime } from '../utils/auxiliaryFunction'
 
 const Timer = () => {
+ 
+    //states to manage time, initial time, running status and editing field with values
+     
+    const [time, setTime] = useState(0); // current time in seconds
+    const [initialTime, setInitialTime] = useState(0); // time initially set by the user
+    const [isRunning, setRunning] = useState(false) // track if the timer is running and paused
+    const [editState, setEditState] = useState({field: null, value: ''});  // State for editing field and value
+
+    //effect to update the progress bar as time counts down
+
+    useEffect( () =>{
+        const progress = initialTime > 0 ? (time/initialTime) * 100 : 0;
+        document.documentElement.style.setProperty('--progress', `${progress}%`);
+    },[time,initialTime]);
+
+    //effect to handle timer countdown when it is running
+    
+
   return (
     <div>Timer</div>
   )
