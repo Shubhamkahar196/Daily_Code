fe3d998e4321a5b49475619cf3e394c101501ba9
Shubham kahar <Shubhamkahar196@gmail.com>
2025-06-23 18:40:33 +0530

    Otp input box work forward and backspave

--- Web/week-13/Tailwind_1/src/components/Project/Otp.jsx
+++ Web/week-13/Tailwind_1/src/components/Project/Otp.jsx
@@ -1,4 +1,7 @@
 import React, {useRef,useState} from 'react'
+import { Button } from './Button';
+
+
 export function Otp(){
    
      const ref1 = useRef(); 
@@ -6,51 +9,77 @@ export function Otp(){
      const ref3 = useRef(); 
      const ref4 = useRef(); 
      const ref5 = useRef(); 
-     const ref6 = useRef(); 
+     const ref6 = useRef();
+  const [disabled, setDisabled] = useState(true);
 
-     const [disabled, setDisabled] = useState(true);
-
-
-    return <div className="flex justify-center">
-     <SubOtpBox reference={ref1} onDone={()=>{
-        ref2.current.focus();
-     }}/>
-
-     <SubOtpBox reference={ref2}  onDone={()=>{
-        ref3.current.focus();
-     }}/>
-
-     <SubOtpBox reference={ref3} onDone={()=>{
-        ref4.current.focus();
-     }}/>
-
-     <SubOtpBox reference={ref4} onDone={()=>{
-        ref5.current.focus();
-     }}/>
-     <SubOtpBox reference={ref5} onDone={()=>{
-        ref6.current.focus();
-     }}/>
-
-     <SubOtpBox reference={ref6} onDone={()=>{
-        // ref6.current.focus();
-        setDisabled(false)
-     }}/>
-
-
-        <br /><br />
-     <Button disabled={disabled}>Signup</Button>
+  return (
+    <div className="flex justify-center">
+      <SubOtpBox reference={ref1} onDone={() => ref2.current.focus()} />
+      <SubOtpBox
+        reference={ref2}
+        onDone={() => ref3.current.focus()}
+        onBack={() => ref1.current.focus()}
+      />
+      <SubOtpBox
+        reference={ref3}
+        onDone={() => ref4.current.focus()}
+        onBack={() => ref2.current.focus()}
+      />
+      <SubOtpBox
+        reference={ref4}
+        onDone={() => ref5.current.focus()}
+        onBack={() => ref3.current.focus()}
+      />
+      <SubOtpBox
+        reference={ref5}
+        onDone={() => ref6.current.focus()}
+        onBack={() => ref4.current.focus()}
+      />
+      <SubOtpBox
+        reference={ref6}
+        onDone={() => setDisabled(false)}
+        onBack={() => ref5.current.focus()}
+      />
+      <br />
+      <br />
+      <Button disabled={disabled}>Signup</Button>
     </div>
-} 
+  );
+}
 
 
-function SubOtpBox({reference, onDone}){
 
-    const [inputBoxValue, setInputBoxValue] = useState("");
-    return <div>
-        <input value={inputBoxValue} ref={reference} onChange={(e)=>{
-            const val = e.target.value;
-            onDone()
-        }} type="text" className="m-1 w-[40px] h-[50px] rounded-xl
-         bg-blue-500 outline-none px-4 text-white" />
+
+function SubOtpBox({ reference, onDone, onBack }) {
+  const [inputBoxValue, setInputBoxValue] = useState('');
+
+
+
+const handleChange = (e) => {
+  const val = e.target.value;
+  if (val === '') {
+    setInputBoxValue('');
+    onBack();
+  } else if (/^\d$/.test(val)) {
+    setInputBoxValue(val);
+    onDone();
+  } else {
+    setInputBoxValue('');
+  }
+};
+
+
+
+  return (
+    <div>
+      <input
+        value={inputBoxValue}
+        ref={reference}
+        onChange={handleChange}
+        type="text"
+        maxLength={1}
+        className="m-1 w-[40px] h-[50px] rounded-xl bg-blue-500 outline-none px-4 text-white"
+      />
     </div>
+  );
 }
