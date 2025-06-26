508103bd0f70fb2a0659189996ede805cdab79df
Shubham kahar <Shubhamkahar196@gmail.com>
2025-06-26 23:01:07 +0530

    Solving problem first

--- Web/Week-14/week-14.1/index.js
+++ Web/Week-14/week-14.1/index.js
@@ -1,4 +1,14 @@
 "use strict";
+// let x: number | string = 1;
+// x = 'shubham'
+// console.log(x)
+// basic types
+function greet(firstName) {
+    console.log("hello world" + firstName);
+}
+greet("shubham");
+// number , string , any
+//  any specail type of function in typeScript
 let x = 1;
-x = 'shubham';
-console.log(x);
+x = true;
+x = "shubham";
--- Web/Week-14/week-14.1/index.ts
+++ Web/Week-14/week-14.1/index.ts
@@ -1,4 +1,20 @@
-let x: number | string = 1;
+// let x: number | string = 1;
  
-x = 'shubham'
-console.log(x)
+// x = 'shubham'
+// console.log(x)
+
+
+// basic types
+// first problem
+function greet(firstName: string){
+    console.log("hello world" + firstName);
+}
+
+greet("shubham");
+
+
+// number , string , any
+//  any specail type of function in typeScript
+let x: any = 1;
+x= true
+x = "shubham"
