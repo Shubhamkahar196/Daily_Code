884296940bc882fa46c57fd6624ce1a7921c916e
Shubham kahar <Shubhamkahar196@gmail.com>
2025-07-05 10:32:31 +0530

    Understanding types in intersection

---
+++ Web/Week-14/week-14.2/src/Typess/basicType.ts
@@ -0,0 +1,48 @@
+
+// defining interface
+interface User1 {
+    name: string,
+    age: number,
+}
+
+// defining types
+// diference = same as interface
+type User2 = {
+    name: string,
+    age: number
+}
+
+//unions and intersections -> work in types and not work in interface
+
+// intersections
+
+type Employee = { 
+    employeeName: string; 
+    startDate: string; 
+}
+
+type Managers = { 
+    managerName: string; 
+    department: string; 
+}
+
+type TeamLead = Employee & Managers;
+
+let e: Employee = { 
+    employeeName: "Shubham", 
+    startDate: "01-10-2025" 
+}
+
+let m: Managers = { 
+    managerName: "shubh", 
+    department: "Computer", 
+}
+
+let teamLead: TeamLead = {
+    employeeName: "Shubham",
+    startDate: "01-10-2025",
+    managerName: "shubh",
+    department: "Computer",
+}
+
+
