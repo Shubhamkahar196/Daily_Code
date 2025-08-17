31bd37242ff357f1544a988374fa5bad91b7c2f2
Shubham kahar <Shubhamkahar196@gmail.com>
2025-08-17 14:29:17 +0530

    added_loigin route

--- Web/week-18/Assignment/src/routes/userRoute.ts
+++ Web/week-18/Assignment/src/routes/userRoute.ts
@@ -7,6 +7,10 @@ import jwt from 'jsonwebtoken'
 const router = Router();
 const Client = new PrismaClient();
 const JWT_SECRET_Key = process.env.JWT_SECRET;
+if (!JWT_SECRET_Key) {
+  throw new Error("JWT_SECRET is not defined");
+}
+
 
 router.post("/signup", async(req,res)=>{
     try{
@@ -34,3 +38,38 @@ router.post("/signup", async(req,res)=>{
         })
     }
 });
+
+// login route
+
+router.post("/login",async(req,res)=>{
+    try{
+        const {email,password} = loginSchema.parse(req.body);
+
+        // finding user through email
+        const user = await Client.user.findUnique({where: {email}})
+        // checking user exist or not
+        if(!user){
+            return res.status(404).json({
+                error: "User not found"
+            })
+        }
+        
+        // if user find then match the password of user
+
+        const isPassword = await bcrypt.compare(password,user.password);
+        // if password not matched
+        if(!isPassword){
+            return res.status(401).json({
+                error: "Invalid credentials"
+            })
+        }
+
+        // checking token
+        const token = jwt.sign({userId: user.id}, JWT_SECRET_Key, {expiresIn: '1h'})
+
+        
+
+    }catch(error){
+        res.status(400).json({ error: 'Invalid input data.' });
+    }
+})
