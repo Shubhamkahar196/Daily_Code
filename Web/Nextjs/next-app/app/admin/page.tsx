41d6a1224d993bfc3c6141b7a6e584278f652180
Shubham kahar <Shubhamkahar196@gmail.com>
2025-08-18 20:54:41 +0530

    Making signup ,signin and blogs page

---
+++ Web/Nextjs/next-app/app/admin/page.tsx
@@ -0,0 +1,6 @@
+export default function admin(){
+    return <div>
+        admin page
+        <h1 className="text-3xl">Big oage</h1>
+    </div>
+}
--- Web/Nextjs/next-app/app/user/page.tsx
+++ Web/Nextjs/next-app/app/user/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
    
    <div>
-    <h1>Hii shubham</h1>
+    <h1>Hii shubham user</h1>
    </div>
   );
 }
