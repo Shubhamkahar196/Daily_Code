6e3f9b205ab0f78753d0d71c01d87f6653a31302
Shubham kahar <Shubhamkahar196@gmail.com>
2025-08-24 23:09:01 +0530

    Starting week-20

--- Web/week-19/project/making-signup-page-with-backend/app/api/v1/signup/route.ts
+++ Web/week-19/project/making-signup-page-with-backend/app/api/v1/signup/route.ts
@@ -1,12 +1,38 @@
+"use client"
 import { NextRequest, NextResponse } from "next/server";
+// import { PrismaClient } from "../src/generated/prisma/client.js";
+import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from ".prisma/client";
+
+// import { PrismaClient } from '../src/generated/prisma/client.js';
+// import { PrismaClient } from '../../prisma/client';
+
+
+
+//  const prismaClient = new PrismaClient();
+
+import prismaClient from "../../../lib/db"
 
 export async function POST( req:NextRequest){
+   
    
 
     const data = await req.json();
 
+    prismaClient.user.create({
+
+    })
+
     console.log(data);
-   
+       
+    prismaClient.user.create({
+        data:{
+            username: data.username,
+            password: data.password
+        }
+    })
+
+
     return NextResponse.json({
         message: "You have been signed up"
     })
--- Web/week-19/project/making-signup-page-with-backend/app/signup/page.tsx
+++ Web/week-19/project/making-signup-page-with-backend/app/signup/page.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-export default function Signin(){
+export default function Signup(){
  const [username, setUsername] = useState("");
  const [password , setPassword] = useState("");
 
@@ -20,14 +20,36 @@ export default function Signin(){
 
          {/* after submitting signup redirect to signin */}
    
-            <button onClick={ async()=>{
-                         await  axios.post("http://localhost:3000/api/v1/signup",{
+            {/* <button onClick={ async()=>{
+                         await axios.post("http://localhost:3000/api/v1/signup",{
                             username,
                             password
                           })
 
                           router.push("/signin")
-            }}>Signup</button>
+            }}>Signup</button> */}
+
+              <button
+  onClick={async () => {
+    try {
+      const response = await axios.post("http://localhost:3000/api/v1/signup", {
+        username,
+        password,
+      });
+      if (response.status === 200) {
+        router.push("/signin");
+      } else {
+        console.error("Error signing up:", response);
+      }
+    } catch (error) {
+      console.error("Error signing up:", error);
+    }
+  }}
+>
+  Signup
+</button>
+
+
         </div>
     </div>
 }
