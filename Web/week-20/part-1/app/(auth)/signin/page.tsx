8742f6361650afc19715a1b3493dc2428790b3de
Shubham kahar <Shubhamkahar196@gmail.com>
2025-08-28 21:41:07 +0530

    Understanding the routing grouds and dynamic routes

---
+++ Web/week-20/part-1/app/(auth)/signin/page.tsx
@@ -0,0 +1,8 @@
+export default function Signin(){
+    return <div>
+      Signin
+      <input type="text" />
+      <input type="password" />
+      <button>Submit</button>
+    </div>
+}
---
+++ Web/week-20/part-1/app/(auth)/singup/page.tsx
@@ -0,0 +1,8 @@
+export default function Signin(){
+    return <div>
+      Signup
+      <input type="text" />
+      <input type="password" />
+      <button>Submit</button>
+    </div>
+}
